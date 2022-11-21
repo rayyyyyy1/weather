b1d7e1d0daeeddd80cd80d2ab45528c682dd0e78
rayyyyyy1 <cojocarucristian65@gmail.com>
2022-11-21 17:10:01 +0200

    hide api key

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const inputvalue=document.getElementById('search').value
 
 async function getData(){
     const city=document.getElementById('search').value
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cd869e0b74d33d03f1f7ce220a8902a6`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cd869e0b74d33d03f1f7ce220a8902a6`, {mode: 'cors' })
     const data = await response.json();
     temperature.textContent=data.main.temp;
     humidity.textContent=data.main.humidity;
