5240d78a3d8273713b0d783528a4f7b28dc3a819
rayyyyyy1 <cojocarucristian65@gmail.com>
2022-11-21 16:58:21 +0200

    added input+search with basic info of the city weather

---
+++ index.js
@@ -0,0 +1,19 @@
+const temperature=document.querySelector('.temp');
+const humidity=document.querySelector('.humidity');
+const feelslike=document.querySelector('.feelslike');
+const searchButton = document.querySelector('.searchButton');
+const inputvalue=document.getElementById('search').value
+
+
+
+async function getData(){
+    const city=document.getElementById('search').value
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cd869e0b74d33d03f1f7ce220a8902a6`)
+    const data = await response.json();
+    temperature.textContent=data.main.temp;
+    humidity.textContent=data.main.humidity;
+    feelslike.textContent=data.main.feels_like;
+}
+searchButton.addEventListener('click',getData);
+
+function celsius()
